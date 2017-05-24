e5d859a10657995828d103140592e94fcc737e31
Arne Brodowski <mail@arnebrodowski.de>
2017-05-24 09:23:06 +0200

    add initial migration

---
+++ generic_confirmation/migrations/0001_initial.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import generic_confirmation.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contenttypes', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='DeferredAction',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('token', models.CharField(max_length=40)),
+                ('valid_until', models.DateTimeField(null=True)),
+                ('confirmed', models.BooleanField(default=False)),
+                ('form_class', models.CharField(max_length=255)),
+                ('form_input', generic_confirmation.fields.PickledObjectField(editable=False)),
+                ('form_prefix', models.CharField(max_length=255, null=True, blank=True)),
+                ('object_pk', models.TextField(null=True)),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType', null=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
