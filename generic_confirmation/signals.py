21c4c784ba83ead6606b589948bbe97703e6b4e4
raunaq <raunaq@sailo.com>
2023-01-19 23:04:16 +0530

    feat(): removed providing_args from django Signals for Django 4 compatibility

--- generic_confirmation/signals.py
+++ generic_confirmation/signals.py
@@ -3,7 +3,13 @@ from django.dispatch import Signal
 # sender is the class which is edited, instance is the DeferedAction instance
 # user is the user who edited the sender instance or None. user is only not
 # None if passed to form form.save(user=request.user) method ... 
-confirmation_required = Signal(providing_args=["instance", "user"])
+# deprecated in Django4
+#confirmation_required = Signal(providing_args=["instance", "user"])
+
+confirmation_required = Signal()
 
 # sender is the class which is edited, instance is the DeferedAction instance
-change_confirmed = Signal(providing_args=["instance"])
+# deprecated in Django4
+#change_confirmed = Signal(providing_args=["instance"])
+
+change_confirmed = Signal()
